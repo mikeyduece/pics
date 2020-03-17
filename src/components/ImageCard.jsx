f8d238d766bca035938286d94f663d58a361df9b
Mike Heft <mikeheft@gmail.com>
2020-03-17 14:57:26 +0000

    Add hooks to dynamically calculate grid-row-end

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 
 const ImageCard = props => {
+  const element = useRef()
+  const [imgRef, setImgRef] = useState(0)
+  const [spans, setSpans] = useState(0)
   const { alt_description, urls, id } = props.image
 
+  useLayoutEffect(() => {
+    const height = element.current.clientHeight
+
+    setImgRef(element.current.addEventListener('load', setSpans))
+    setSpans(Math.ceil(height / 150))
+  }, [])
+
   return (
-    <div>
-      <img key={ id } src={ urls.regular } alt={ alt_description } />
+    <div key={ id } style={ { gridRowEnd: `span ${ spans }` } }>
+      <img ref={ element } src={ urls.regular } alt={ alt_description } />
     </div>
   )
 }
