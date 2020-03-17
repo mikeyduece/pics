4ff8f94305c0abbe1c5cb47e5968d7f4da4d5bb7
Mike Heft <mikeheft@gmail.com>
2020-03-17 11:31:47 +0000

    Add imageList scss

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,13 +1,14 @@
+import './ImageList.scss'
 import React from 'react'
 
 const ImageList = props => {
   console.log(props.images)
-  const images = props.images.map(({alt_description, id, urls}) => {
+  const images = props.images.map(({ alt_description, id, urls }) => {
     return <img key={ id } src={ urls.regular } alt={ alt_description } />
   })
 
   return (
-    <div>
+    <div className='img-list'>
       { images }
     </div>
   )
