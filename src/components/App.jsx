e536034e22d78a2082bdf302fae1b7bccc4e0af4
Mike Heft <mikeheft@gmail.com>
2020-03-17 11:00:39 +0000

    Refactor App#onSearchSubmit to use async/ inc unsplash client

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import axios from 'axios'
+import React    from 'react'
+import unsplash from '../api/unsplash'
 
 import SearchBar from './SeacrhBar'
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const resposne = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: `Client-ID ${ process.env.REACT_APP_UNSPLASH_CLIENT_KEY }`
-      }
+  state = { images: [] }
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
     })
 
-    console.log(resposne.data.results)
+    this.setState({ images: response.data.results })
   }
 
   render() {
     return (
       <div className='ui container' style={ { marginTop: '10px' } }>
         <SearchBar onSubmit={ this.onSearchSubmit }/>
+        Found: { this.state.images.length } images
       </div>
     )
   }
