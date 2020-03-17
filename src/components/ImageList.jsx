837b6a64d9a0814b1a65b178fa6269fdbb9c63d3
Mike Heft <mikeheft@gmail.com>
2020-03-17 14:57:59 +0000

    Add key to ImageCard inside iteration

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,11 +1,10 @@
 import './ImageList.scss'
 
 import React     from 'react'
-
 import ImageCard from './ImageCard'
 
 const ImageList = props => {
-  const images = props.images.map(image => <ImageCard image={ image } />)
+  const images = props.images.map(image => <ImageCard key={ image.id } image={ image } />)
 
   return (
     <div className='img-list'>
