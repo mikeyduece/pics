0c76a2be392809c1d81a8314a18828750492510b
Mike Heft <mikeheft@gmail.com>
2020-03-17 05:39:35 +0000

    Add onFormSubmit function

--- src/components/SeacrhBar.jsx
+++ src/components/SeacrhBar.jsx
@@ -3,16 +3,23 @@ import React from 'react'
 class SeacrhBar extends React.Component {
   state = {term: ''}
 
+  onFormSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(this.state.term)
+  }
+
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form className='ui form' onSubmit={ this.onFormSubmit }>
           <div className='field'>
             <label>Image Search</label>
             <input type='text'
+                   value={ this.state.term }
                    onChange={ e => this.setState({term: e.target.value}) }
-                   value={this.state.term}
             />
+
           </div>
         </form>
       </div>
