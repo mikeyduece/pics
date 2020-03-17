8b4df53e78cc577491145167ab6cd2342738a8b7
Mike Heft <mikeheft@gmail.com>
2020-03-17 06:12:05 +0000

    Start to add axios to App to call to unsplash

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import axios from 'axios'
 
 import SearchBar from './SeacrhBar'
 
-const App = () => {
-  return (
-    <div className='ui container' style={{marginTop: '10px'}}>
-      <SearchBar/>
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unspash.com/search/photos', {
+      headers: {
+        Authorization: 'Client-ID wCO6suYv-CBZawFHgp-hZq1xr-0sLVyZAx67HimMCjo'
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='ui container' style={ {marginTop: '10px'} }>
+        <SearchBar onSubmit={ this.onSearchSubmit }/>
+      </div>
+    )
+  }
 }
 
 export default App
